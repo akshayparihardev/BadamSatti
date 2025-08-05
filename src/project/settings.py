56d83f1dfba3ba555df1b82b0ac394344bc729c6
Akshay Parihar <akshayparihar9310@gmail.com>
2025-08-06 01:56:42 +0530

    Updated the Settings

--- src/project/settings.py
+++ src/project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,os.path.join('templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
